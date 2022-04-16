de0be9edbc519824461d71c8cf4671f276f510db
Brandon Brown Mac <branjbrown20@gmail.com>
2022-04-16 16:19:49 -0400

    Filling the deck with ranks and suites.

--- BrandonBrownch11_ex4_CardDeck/src/CardDeckApp.java
+++ BrandonBrownch11_ex4_CardDeck/src/CardDeckApp.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class CardDeckApp {
 
     public static void main(String[] args) {
@@ -18,22 +20,23 @@ public class CardDeckApp {
     private static String[] getDeck() {
          String[] deck = new String[52];
         // add card Suites Here
+        // Step 2 - Create a deck of cards with 2 string arrays; one for suites and one for rank.
 
         String[] suites = {
             "Hearts", "Diamonds", "Clubs", "Spades"
         };
 
         String[] ranks = {
-            "2 of", "3 of", "4 of", "5 of", "6 of", "7 of", "8 of",
-            "9 of", "10 of", "Jack of", "Queen of", "King of", "Ace of"
+            "2 of ", "3 of ", "4 of ", "5 of ", "6 of ", "7 of ", "8 of ",
+            "9 of ", "10 of ", "Jack of ", "Queen of ", "King of ", "Ace of "
         };
 
         for(int i = 0; i < ranks.length; i++) { 
             for(int j = 0; j < suites.length;j++) { 
-                System.out.println(ranks[i] + " " + suites[j]);
+               Arrays.fill(deck, ranks[i] + suites[j] + "|"); // Fills the deck with ranks and suites.
+               // System.out.println(deck[1]); // Successful test of the fill method
             }
         }
-        
         return deck;
     }
 
