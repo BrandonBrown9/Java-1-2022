75f076fe4c890687089b55fd7f65f06b736c1d6a
Brandon Brown Mac <branjbrown20@gmail.com>
2022-04-19 13:01:57 -0400

    Adding style requirements for the grid. Adding detailed comments

--- BrandonBrownch17_ex1_MPGFX/src/MPGApplication.java
+++ BrandonBrownch17_ex1_MPGFX/src/MPGApplication.java
@@ -1,11 +1,12 @@
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-// import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
@@ -13,40 +14,42 @@ public class MPGApplication extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        // Step 4- Create grid with labels and text fields.
-        GridPane grid = new GridPane();
-        grid.add(new Label("Miles:"), 0,1);
-        grid.add(new Label("Gallons:"), 0,2);
-        grid.add(new Label("MPG:"), 0,3);
+        // Step 4 - Create grid with labels and text fields.
+        // Step 6 - Give grid 25px from the sides. Center the grid. Give 10px between the rows.
+            GridPane grid = new GridPane();
+            grid.setAlignment(Pos.TOP_CENTER);
+            grid.setPadding(new Insets(0, 25, 0, 25));
+            grid.setHgap(10);
+            grid.setVgap(10);
+            grid.add(new Label("Miles:"), 0,1);
+            grid.add(new Label("Gallons:"), 0,2);
+            grid.add(new Label("MPG:"), 0,3);
 
-        TextField miles = new TextField();
-        grid.add(miles, 1, 1);
-        TextField gallons = new TextField();
-        grid.add(gallons, 1, 2);
-        TextField mpg = new TextField();
-        mpg.setEditable(false); // This field is set to read only.
-        grid.add(mpg, 1, 3);
+        // Initialize the Text Fields
+            TextField miles = new TextField();
+            grid.add(miles, 1, 1);
+            TextField gallons = new TextField();
+            grid.add(gallons, 1, 2);
+            TextField mpg = new TextField();
+            mpg.setEditable(false); // This field is set to read only.
+            grid.add(mpg, 1, 3);
 
-        // Step 6 - Styles for columns
-        
+        // Button functionality and properties    
+            Button btn = new Button();
+            grid.add(btn, 1, 4); // Give the button a fixed position AFTER the labels and fields
+            btn.setText("Calculate");
+            btn.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent event) {
+                    System.out.println("Calculate");
+                }
+            });
 
-
-        Button btn = new Button();
-        btn.setText("Calculate");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Calculate");
-            }
-        });
-        
-        grid.getChildren().add(btn);
-        
-        Scene scene = new Scene(grid, 300, 250);
-
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        // Instantiate the scence    
+            Scene scene = new Scene(grid, 300, 250);
+            primaryStage.setTitle("Miles Per Gallon Calculator");
+            primaryStage.setScene(scene);
+            primaryStage.show();        
     } // End of MPGApplication
 
     public static void main(String[] args) {
