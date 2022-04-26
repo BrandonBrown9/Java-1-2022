08dfedea8b94802076eadc1198c4b15a6ea75d41
Brandon Brown Mac <branjbrown20@gmail.com>
2022-04-26 12:44:43 -0400

    Adding Calculate and Exit Button. Method for exit button added.

--- BrandonBrownPizzaApp/src/PizzaApplication.java
+++ BrandonBrownPizzaApp/src/PizzaApplication.java
@@ -1,6 +1,7 @@
 // Brandon Brown 4/25/2022
 
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
@@ -29,13 +30,28 @@ public class PizzaApplication extends Application {
     grid.add(new Label("Qty"), 0,2);
     grid.add(new Label("Price"),0,3);
 
+    // Validation for fields (Pages 555 ,557, 561)
+
     // Grid Buttons
+    Button bbCalculate = new Button("Calculate");
+    grid.add(bbCalculate, 2, 4);
+    Button bbExit = new Button("Exit");
+    bbExit.setOnAction(event -> bbExitButtonClicked());
+    grid.add(bbExit, 3, 4);
+
 
     // Instantiate Scene
     Scene scene = new Scene(grid, 300, 250);
     primaryStage.setTitle("Brandon's Pizza");
     primaryStage.setScene(scene);
     primaryStage.show();
+    }
+
+    // Calculate button method
+
+    // Exit button method
+    private void bbExitButtonClicked() {
+        System.exit(0);
     }
 
     // Main Method
